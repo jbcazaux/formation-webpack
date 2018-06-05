25a7fafb5810b92d3074c97ae80eae82afc9afa8
jbcazaux <jbcazaux@gmail.com>
2018-06-05 16:44:18 +0200

    example 4

---
+++ examples/04 - calc/calc.js
@@ -0,0 +1,12 @@
+class Calc {
+
+  add = (a, b) => a + b;
+
+  /*async *oneAndTwo() {
+    await 1;
+    yield 2;
+  }*/
+
+}
+
+export default Calc;
---
+++ examples/04 - calc/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    bundle: './src/main.js'
+  },
+  output: {
+    path: path.resolve(__dirname, '../', 'build'),
+    filename: '[name].js',
+    publicPath: '/'
+  },
+  resolve: {extensions: ['.js', '.es6']},
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+};
