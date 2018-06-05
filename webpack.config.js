729ab2f22781ecbaa307f135e017316ee5f7e2aa
jbcazaux <jbcazaux@gmail.com>
2018-06-05 18:54:45 +0200

    webpack dev server clean plugin

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    bundle: './src/main.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].js',
+    publicPath: '/'
+  },
+  resolve: {extensions: ['.js']},
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+    new CleanWebpackPlugin(['build'], {verbose: true}) // clean repertoire build
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'), // si on veut acceder a des ressources non webpack (favico ?)
+    publicPath: '/',
+    open: true
+  }
+};
